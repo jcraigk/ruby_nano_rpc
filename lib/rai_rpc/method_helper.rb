8246733b6e6b25ddd2952d9d203ba83183318457
Justin Craig-Kuhn <jcraigk@gmail.com>
2018-01-20 11:25:51 -0800

    Refactor method helper

--- lib/rai_rpc/method_helper.rb
+++ lib/rai_rpc/method_helper.rb
@@ -2,40 +2,45 @@
 module RaiRpc::MethodHelper
   def instantiate_methods
     model_methods.each do |method_name, param_signature|
-      define_rpc_method(
-        method_name,
-        method_name,
-        param_signature,
-        model_params
-      )
-
-      # Also define shortcut method by dropping method prefix
-      next unless (method_name = method_name.to_s).start_with?(method_prefix)
-      define_rpc_method(
-        method_name[method_prefix.size..-1],
-        method_name,
-        param_signature,
-        model_params
-      )
+      define_rpc_methods(method_name, param_signature, model_params)
     end
   end
 
-  def define_rpc_method(method_name, action, param_signature, model_params)
-    self.class.send(:define_method, method_name) do |options = {}|
-      model_params.each { |k, v| options[k] ||= send(v) }
-      if param_signature.is_a?(Hash)
-        missing_params = param_signature[:required] -
-                         options.keys.map(&:to_sym)
-        ensure_parameters!(missing_params)
+  def define_rpc_methods(method_name, param_signature, model_params)
+    method_list(method_name.to_s).each do |props|
+      self.class.send(:define_method, props[:method]) do |options = {}|
+        validate_parameters!(param_signature, options)
+
+        model_params.each { |k, v| options[k] ||= send(v) }
+        RaiRpc::Client.instance.query(props[:action], options)
       end
-
-      RaiRpc::Client.instance.query(action, options)
     end
   end
 
-  def ensure_parameters!(missing_params)
+  def validate_parameters!(param_signature, options)
+    return unless param_signature.is_a?(Hash)
+    missing_params = param_signature[:required] -
+                     options.keys.map(&:to_sym)
+    ensure_required_parameters!(missing_params)
+  end
+
+  def ensure_required_parameters!(missing_params)
     return unless missing_params.any?
     raise RaiRpc::MissingParameters,
           "Missing required parameter(s): #{missing_params.join(', ')}"
+  end
+
+  def method_list(method_name)
+    method_list = [{ method: method_name, action: method_name }]
+    # Provide abbreviated convenience method if redundant prefix present
+    # `account_balance` => `balance`
+    if method_name.start_with?(method_prefix)
+      method_list << {
+        method: method_name[method_prefix.size..-1],
+        action: method_name
+      }
+    end
+
+    method_list
   end
 end
