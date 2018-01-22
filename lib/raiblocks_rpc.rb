eea1c695b7b8296cb5f5cdc6d9dbb85c6dd95410
Justin Craig-Kuhn <jcraigk@gmail.com>
2018-01-21 21:44:47 -0800

    Include version first, bump to 0.3.0

--- lib/raiblocks_rpc.rb
+++ lib/raiblocks_rpc.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
+require 'raiblocks_rpc/version'
 require 'raiblocks_rpc/client'
 require 'raiblocks_rpc/errors'
 require 'raiblocks_rpc/proxy'
 require 'raiblocks_rpc/response'
-require 'raiblocks_rpc/version'
 require 'raiblocks_rpc/proxies/account'
 require 'raiblocks_rpc/proxies/accounts'
 require 'raiblocks_rpc/proxies/network'
