bbf507faca3f5e7210bc353015fad02e6d775c66
Justin Craig-Kuhn <jcraigk@gmail.com>
2018-05-24 14:01:58 -0700

    Wording tweak in test

--- spec/errors_spec.rb
+++ spec/errors_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 require 'spec_helper'
 
-RSpec.describe 'Nano errors' do
+RSpec.describe 'NanoRpc errors' do
   describe NanoRpc::Error do
     it 'is a StandardError' do
       expect(subject).to be_a(StandardError)
